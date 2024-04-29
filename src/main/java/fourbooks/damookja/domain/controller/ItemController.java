300f8731e93328c0173cbe8917b2f258b210a914
naegwon <joonwoo9192@gmail.com>
2024-04-29 17:57:26 +0900

    ResponseEntity 변경

--- src/main/java/fourbooks/damookja/domain/controller/ItemController.java
+++ src/main/java/fourbooks/damookja/domain/controller/ItemController.java
@@ -21,22 +21,22 @@ public class ItemController {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @GetMapping
     public ResponseEntity<List<Item>> getAll() {
-        return ResponseEntity.ok(itemService.findAll());
+        return new ResponseEntity<>(itemService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Item> getItemById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(itemService.findById(id));
+        return new ResponseEntity<>(itemService.findById(id), HttpStatus.OK);
     }
 
     @GetMapping("/name/{name}")
     public ResponseEntity<List<Item>> getItemByName(@PathVariable("name") String name) {
-        return ResponseEntity.ok(itemService.findByNameContaining(name));
+        return new ResponseEntity<>(itemService.findByNameContaining(name), HttpStatus.OK);
     }
 
     @PostMapping
@@ -50,13 +50,13 @@ public class ItemController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Item> updateItem(@PathVariable("id") Long id, @RequestBody Item updatedItem) {
-        Item item = itemService.updateItem(id, updatedItem);
-        return ResponseEntity.ok(item);
+    public ResponseEntity<Item> updateItem(@PathVariable("id") Long id, @RequestBody Item updateItem) {
+        Item updatedItem = itemService.updateItem(id, updateItem);
+        return new ResponseEntity<>(updatedItem, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Long> deleteItem(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(itemService.deleteItem(id));
+        return new ResponseEntity<>(itemService.deleteItem(id), HttpStatus.OK);
     }
 }
